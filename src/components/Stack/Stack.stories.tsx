2c5298be358396f13f225e5b418593a747533bb2
CristianRojas-47 <crisrouges47@gmail.com>
2025-01-03 16:03:01 -0500

    feat(stack): add stack component to inubekit

---
+++ src/components/Stack/Stack.stories.tsx
@@ -0,0 +1,35 @@
+import { Stack, IStack } from "./index";
+import { props } from "./props";
+import { Squares } from "./squares";
+
+const story = {
+  title: "layout/Stack",
+  components: [Stack],
+  argTypes: props,
+};
+
+const Default = (args: IStack) => (
+  <Stack {...args}>
+    {Array.isArray(args.children)
+      ? args.children.map((item, index) => <Squares key={index} item={item} />)
+      : args.children}
+  </Stack>
+);
+
+Default.args = {
+  children: [...Array(6 + 1).keys()].slice(1),
+  gap: "10px",
+  wrap: "wrap",
+  direction: "row",
+  justifyContent: "unset",
+  alignItems: "initial",
+  alignContent: "unset",
+  height: "100%",
+  width: "100%",
+  margin: "0px",
+  padding: "0px 20px",
+};
+
+export default story;
+
+export { Default };
---
+++ src/components/Stack/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledFlex = styled.div`
+  display: flex;
+  justify-content: ${({ $justifyContent }) => $justifyContent};
+  align-items: ${({ $alignItems }) => $alignItems};
+  align-content: ${({ $alignContent }) => $alignContent};
+  flex-direction: ${({ $direction }) => $direction};
+  flex-wrap: ${({ $wrap }) => $wrap};
+  height: ${({ $height }) => $height};
+  width: ${({ $width }) => $width};
+  gap: ${({ $gap }) => $gap};
+  margin: ${({ $margin }) => $margin};
+  padding: ${({ $padding }) => $padding};
+`;
+
+export { StyledFlex };
