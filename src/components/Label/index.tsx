fb94f4650df3514927944be70e6b2036fc143934
CristianRojas-47 <crisrouges47@gmail.com>
2025-01-07 10:21:06 -0500

    feat(label): add label component to inubekit

---
+++ src/components/Label/index.tsx
@@ -0,0 +1,59 @@
+import { ILabelSize } from "./props";
+import { Text } from "../Text";
+import { ITextAppearance } from "../Text/props";
+import { tokens } from "./tokens";
+import { useContext } from "react";
+import { ThemeContext } from "styled-components";
+
+interface ILabel {
+  children?: React.ReactNode;
+  disabled?: boolean;
+  focused?: boolean;
+  htmlFor?: string;
+  invalid?: boolean;
+  margin?: string;
+  padding?: string;
+  size?: ILabelSize;
+}
+
+const Label = (props: ILabel) => {
+  const {
+    children,
+    disabled = false,
+    focused = false,
+    htmlFor,
+    invalid = false,
+    margin = "0px",
+    padding = "0px",
+    size = "large",
+  } = props;
+
+  const theme = useContext(ThemeContext) as { label: typeof tokens };
+
+  const getAppearance = (): ITextAppearance => {
+    const { color } = theme?.label?.content || tokens.content;
+
+    if (invalid) return color.invalid as unknown as ITextAppearance;
+    if (focused) return color.focus as unknown as ITextAppearance;
+    if (disabled) return color.disabled as unknown as ITextAppearance;
+    return color.regular as unknown as ITextAppearance;
+  };
+
+  return (
+    <label htmlFor={htmlFor}>
+      <Text
+        appearance={getAppearance()}
+        margin={margin}
+        padding={padding}
+        size={size}
+        type="label"
+        weight="bold"
+      >
+        {children}
+      </Text>
+    </label>
+  );
+};
+
+export { Label };
+export type { ILabel };
---
+++ src/components/Label/tokens.ts
@@ -0,0 +1,12 @@
+const tokens = {
+  content: {
+    color: {
+      regular: "dark",
+      disabled: "dark",
+      focus: "primary",
+      invalid: "danger",
+    },
+  },
+};
+
+export { tokens };
