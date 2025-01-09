0a9be928e040c497d62bc8c53099e966744c5b70
CristianRojas-47 <crisrouges47@gmail.com>
2025-01-09 16:02:59 -0500

    feat(checkbox): add checkbox component to inubekit

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,102 @@
+import { Label } from "../Label";
+import { Stack } from "../Stack";
+import { Text } from "../Text";
+import { useContext } from "react";
+import { MdCheck, MdRemove } from "react-icons/md";
+import { ThemeContext } from "styled-components";
+import {
+  StyledCheckbox,
+  StyledLabel,
+  StyledPreventSelectableLabel,
+  StyledSpan,
+} from "./styles";
+import { tokens } from "./tokens";
+
+interface ICheckbox {
+  id?: string;
+  name?: string;
+  value: string;
+  checked?: boolean;
+  indeterminate?: boolean;
+  label?: string;
+  disabled?: boolean;
+  required?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Checkbox = (props: ICheckbox) => {
+  const {
+    id = "checkbox",
+    name,
+    value,
+    checked = false,
+    indeterminate = false,
+    label,
+    disabled = false,
+    required = false,
+    onChange,
+  } = props;
+
+  const theme = useContext(ThemeContext) as { checkbox: typeof tokens };
+  const disabledAppearance =
+    theme?.checkbox?.vector?.color?.disabled || tokens.vector.color.disabled;
+  const indeterminateAppearance =
+    theme?.checkbox?.vector?.color?.indeterminate ||
+    tokens.vector.color.indeterminate;
+  const checkedAppearance =
+    theme?.checkbox?.vector?.color?.checked || tokens.vector.color.checked;
+
+  return (
+    <Stack direction="row" alignItems="center" gap="10px" width="fit-content">
+      <StyledLabel>
+        <StyledCheckbox
+          type="checkbox"
+          id={id}
+          name={name}
+          value={value}
+          checked={checked || indeterminate}
+          disabled={disabled}
+          required={required}
+          onChange={onChange}
+          ref={(el: { indeterminate: boolean }) => {
+            if (el) el.indeterminate = indeterminate;
+          }}
+        />
+        <StyledSpan
+          checked={checked}
+          $indeterminate={indeterminate}
+          disabled={disabled}
+        >
+          {indeterminate ? (
+            <MdRemove
+              color={disabled ? disabledAppearance : indeterminateAppearance}
+            />
+          ) : (
+            checked && (
+              <MdCheck
+                color={disabled ? disabledAppearance : checkedAppearance}
+              />
+            )
+          )}
+        </StyledSpan>
+      </StyledLabel>
+      {label && (
+        <StyledPreventSelectableLabel>
+          <Label htmlFor={id} disabled={disabled}>
+            <Stack direction="row" alignItems="center" gap="4px">
+              {label}
+              {required && (
+                <Text type="label" appearance="danger" size="small">
+                  *
+                </Text>
+              )}
+            </Stack>
+          </Label>
+        </StyledPreventSelectableLabel>
+      )}
+    </Stack>
+  );
+};
+
+export { Checkbox };
+export type { ICheckbox };
---
+++ src/components/Checkbox/styles.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import { tokens } from "./tokens";
+
+const StyledCheckbox = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+
+  &:checked + span {
+    border-color: ${({ theme }) =>
+      theme?.checkbox?.border?.color?.checked || tokens.border.color.checked};
+    background-color: ${({ theme }) =>
+      theme?.checkbox?.background?.color?.checked ||
+      tokens.background.color.checked};
+  }
+
+  &:focus + span {
+    outline: 2px solid
+      ${({ theme }) =>
+        theme?.checkbox?.outline?.color?.hover || tokens.outline.color.hover};
+    outline-offset: 2px;
+  }
+
+  &:disabled + span {
+    border-color: ${({ theme }) =>
+      theme?.checkbox?.border?.color?.disabled || tokens.border.color.disabled};
+    background-color: ${({ theme }) =>
+      theme?.checkbox?.background?.color?.disabled ||
+      tokens.background.color.disabled};
+  }
+`;
+
+const StyledSpan = styled.span`
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  border-radius: 2px;
+  border: 2px solid
+    ${({ theme, checked, $indeterminate }) =>
+      checked || $indeterminate
+        ? theme?.checkbox?.border?.color?.checked || tokens.border.color.checked
+        : theme?.checkbox?.border?.color?.active || tokens.border.color.active};
+  background-color: ${({ theme, checked, $indeterminate }) =>
+    checked || $indeterminate
+      ? theme?.checkbox?.background?.color?.checked ||
+        tokens.background.color.checked
+      : theme?.checkbox?.background?.color?.active ||
+        tokens.background.color.active};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${({ disabled }) =>
+    disabled &&
+    `
+    border-color: ${tokens.border.color.disabled};
+    background-color: ${tokens.background.color.disabled};
+  `}
+`;
+
+const StyledLabel = styled.label`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledPreventSelectableLabel = styled.label`
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
+
+export {
+  StyledCheckbox,
+  StyledLabel,
+  StyledPreventSelectableLabel,
+  StyledSpan,
+};
