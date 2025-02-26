f5d0a4c9b081e3944c84bff7876ac650cf6a86ed
Fernando Cañas <fernando.canas@outlook.com>
2025-02-26 17:48:12 -0500

    feat: create new Box component

---
+++ src/components/Box/Box.stories.tsx
@@ -0,0 +1,46 @@
+import { Box, IBox } from ".";
+
+import { Button } from "../Button";
+import { Text } from "../Text";
+import { Stack } from "../Stack";
+import { ThemeProvider } from "styled-components";
+import { palette } from "../Foundations/palette";
+
+const story = {
+  title: "layout/Box",
+  components: Box,
+};
+
+const Default = (args: IBox) => <Box {...args} />;
+Default.args = {
+  margin: "0px",
+  padding: "16px",
+  borderRadius: "8px",
+};
+
+const Composition = (args: IBox) => (
+  <Box {...args}>
+    <Stack direction="column" gap="8px">
+      <Text>The quick brown fox jumps over the lazy dog</Text>
+      <Button appearance="gray" variant="outlined">
+        Confirm
+      </Button>
+    </Stack>
+  </Box>
+);
+
+const Themed = (args: IBox) => (
+  <ThemeProvider theme={theme}>
+    <Box {...args} />
+  </ThemeProvider>
+);
+const theme = {
+  box: {
+    border: {
+      color: palette.teal.T300,
+    },
+  },
+};
+
+export { Default, Composition, Themed };
+export default story;
---
+++ src/components/Box/tokens.ts
@@ -0,0 +1,9 @@
+import { palette } from "../Foundations/palette";
+
+const box = {
+  border: {
+    color: palette.neutral.N40,
+  },
+};
+
+export { box };
