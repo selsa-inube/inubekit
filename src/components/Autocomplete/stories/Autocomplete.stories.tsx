5484cd3a5e655e3cb94d842eac18d8b3da1070ab
CristianRojas-47 <crisrouges47@gmail.com>
2025-04-24 11:13:18 -0500

    fix(autocomplete): adjust filter to autocomplete

--- src/components/Autocomplete/stories/Autocomplete.stories.tsx
+++ src/components/Autocomplete/stories/Autocomplete.stories.tsx
@@ -13,10 +13,18 @@ const story = {
 };
 
 const optionsMock = [
-  { id: "op1", label: "Option 1", value: "option-1" },
-  { id: "op2", label: "Option 2", value: "option-2" },
-  { id: "op3", label: "Option 3", value: "option-3" },
-  { id: "op4", label: "Option 4", value: "option-4" },
+  { id: "op1", label: "Argentina", value: "option-1" },
+  { id: "op2", label: "Bolivia", value: "option-2" },
+  { id: "op3", label: "Brasil", value: "option-3" },
+  { id: "op4", label: "Chile", value: "option-4" },
+  { id: "op5", label: "Colombia", value: "option-5" },
+  { id: "op6", label: "Ecuador", value: "option-6" },
+  { id: "op7", label: "Guyana", value: "option-7" },
+  { id: "op8", label: "Paraguay", value: "option-8" },
+  { id: "op9", label: "Perú", value: "option-9" },
+  { id: "op10", label: "Surinam", value: "option-10" },
+  { id: "op11", label: "Uruguay", value: "option-11" },
+  { id: "op12", label: "Venezuela", value: "option-12" },
 ];
 
 const Default = (args: IAutocomplete) => <AutocompleteController {...args} />;
