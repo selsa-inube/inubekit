36533052db27be14e338c66a472ec681c71809cf
CristianRojas-47 <crisrouges47@gmail.com>
2025-04-30 09:48:25 -0500

    fix(input-money): format initial value in Moneyfield when set via props

--- src/components/Input/Moneyfield/index.tsx
+++ src/components/Input/Moneyfield/index.tsx
@@ -1,7 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { MdOutlineAttachMoney } from "react-icons/md";
 import { IInput } from "../Input";
 import { InputUI } from "../Input/interface";
+
+const currencyFormatter = new Intl.NumberFormat("es-CO", {
+  style: "currency",
+  currency: "COP",
+  minimumFractionDigits: 0,
+});
 
 const Moneyfield = (props: IInput) => {
   const {
@@ -10,31 +15,28 @@ const Moneyfield = (props: IInput) => {
     onChange,
     ...rest
   } = props;
+
   const formatMoney = (amount: string) => {
     if (!amount) return "";
 
     const numberValue = parseFloat(amount.replace(/[^0-9]/g, ""));
-    if (isNaN(numberValue)) {
-      return "";
-    }
-
-    const formatter = new Intl.NumberFormat("es-CO", {
-      style: "currency",
-      currency: "COP",
-      minimumFractionDigits: 0,
-    });
-
-    return formatter.format(numberValue);
+    return isNaN(numberValue) ? "" : currencyFormatter.format(numberValue);
   };
 
+  const formattedValue = formatMoney(value.toString());
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.target as HTMLInputElement;
+    const input = e.target;
     const rawValue = input.value.replace(/[^0-9]/g, "");
     const formattedValue = formatMoney(rawValue);
-    const cursorPosition = input.selectionStart;
+
+    const cursorPosition = input.selectionStart ?? 0;
+
     input.value = formattedValue;
-    const newCursorPosition =
-      cursorPosition! + (formattedValue.length - rawValue.length);
+
+    const diff = formattedValue.length - rawValue.length;
+    const newCursorPosition = cursorPosition + diff;
+
     input.setSelectionRange(newCursorPosition, newCursorPosition);
 
     if (onChange) {
@@ -49,7 +51,14 @@ const Moneyfield = (props: IInput) => {
     }
   };
 
-  return <InputUI {...rest} iconAfter={iconAfter} onChange={handleChange} />;
+  return (
+    <InputUI
+      {...rest}
+      value={formattedValue}
+      iconAfter={iconAfter}
+      onChange={handleChange}
+    />
+  );
 };
 
 export { Moneyfield };
