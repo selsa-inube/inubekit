23b831042e5e00e86efc062a416a3c02745428c3
CristianRojas-47 <crisrouges47@gmail.com>
2025-06-06 13:58:14 -0500

    feat(input): adjust text styles in the input component

--- src/components/Input/Input/styles.js
+++ src/components/Input/Input/styles.js
@@ -52,20 +52,22 @@ const StyledInput = styled.input`
   padding: 0;
   margin: 0;
   height: 24px;
-  font-weight: 400;
   border: none;
   width: 100%;
   background-color: transparent;
   font-family: ${({ theme }) =>
     theme?.typography?.body?.large?.font || inube.typography.body.large.font};
   font-size: ${({ theme }) =>
-    theme?.typography?.body?.large?.font || inube.typography.body.large.size};
+    theme?.typography?.body?.large?.size || inube.typography.body.large.size};
   line-height: ${({ theme }) =>
-    theme?.typography?.body?.large?.font ||
+    theme?.typography?.body?.large?.lineHeight ||
     inube.typography.body.large.lineHeight};
   letter-spacing: ${({ theme }) =>
-    theme?.typography?.body?.large?.font ||
+    theme?.typography?.body?.large?.tracking ||
     inube.typography.body.large.tracking};
+  font-weight: ${({ theme }) =>
+    theme?.typography?.body?.large?.weight ||
+    inube.typography.body.large.weight};
   color: ${({ $disabled, theme }) =>
     $disabled
       ? theme?.input?.content?.color?.disabled || tokens.content.color.disabled
