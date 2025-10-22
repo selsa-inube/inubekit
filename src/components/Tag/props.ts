2e9bdfc9371d94e8ac9c62785147962619f17f5f
Yamil Escorcia Infante <71434195+Yamil2717@users.noreply.github.com>
2025-10-22 08:06:58 -0500

    docs(Label, Tag, Text): refactor stories and props for new standar

--- src/components/Tag/props.ts
+++ src/components/Tag/props.ts
@@ -21,41 +21,35 @@ const parameters = {
 
 const props = {
   appearance: {
-    control: "select",
+    control: { type: "select" as const },
     options: appearances,
     description:
       "Controls the background color of the tag. The available options are predefined styles such as primary, success, danger, etc. Defaults to 'primary'.",
-    type: { name: "enum", value: appearances },
   },
   id: {
     description:
       "A unique identifier for the tag component. This is an optional field.",
-    type: { name: "string" },
   },
   label: {
     description:
       "Controls the text that the tag will display. This is a required field.",
-    type: { name: "string" },
   },
   onClose: {
     description:
       "Callback function that is triggered when the close icon is clicked. This function receives a MouseEvent as an argument.",
-    type: { name: "function", params: [{ name: "MouseEvent" }] },
   },
   removable: {
     description:
       "Determines if the tag can be removed. When true, a close icon is displayed. Defaults to false.",
-    type: { name: "boolean" },
   },
   displayIcon: {
     description: "Determines whether the tag should be accompanied by an icon.",
-    type: { name: "boolean" },
   },
   icon: {
+    control: { disable: true },
     description:
       "Corresponds to the icon that you want to show in the component " +
       "Requires displayIcon to be true to be visible.",
-    type: { name: "ReactElement" },
   },
 };
 
