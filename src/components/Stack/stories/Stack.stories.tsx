e6bd54df94315ed89024ca25eb08d11f8f283edd
Yamil Escorcia Infante <71434195+Yamil2717@users.noreply.github.com>
2025-10-26 13:51:27 -0500

    docs(Stack, StackItem, Caption, Col): stories to new standard

--- src/components/Stack/stories/Stack.stories.tsx
+++ src/components/Stack/stories/Stack.stories.tsx
@@ -1,38 +1,41 @@
-import { Stack, IStack } from "../index";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Stack } from "../index";
 import { props } from "../props";
 import { Squares } from "./Squares";
 
-const story = {
+const meta = {
   title: "layout/Stack",
-  components: [Stack],
+  component: Stack,
   argTypes: props,
+} satisfies Meta<typeof Stack>;
+
+type Story = StoryObj<typeof Stack>;
+
+const Default: Story = {
+  args: {
+    children: [...Array(6 + 1).keys()].slice(1),
+    as: "div",
+    gap: "10px",
+    wrap: "wrap",
+    direction: "row",
+    justifyContent: "unset",
+    alignItems: "initial",
+    alignContent: "unset",
+    height: "100%",
+    width: "100%",
+    margin: "0px",
+    padding: "0px 20px",
+  },
+  render: (args) => (
+    <Stack {...args}>
+      {Array.isArray(args.children)
+        ? args.children.map((item, index) => (
+            <Squares key={index} as={args.as || "div"} item={item} />
+          ))
+        : args.children}
+    </Stack>
+  ),
 };
 
-const Default = (args: IStack) => (
-  <Stack {...args}>
-    {Array.isArray(args.children)
-      ? args.children.map((item, index) => (
-          <Squares key={index} as={args.as || "div"} item={item} />
-        ))
-      : args.children}
-  </Stack>
-);
-
-Default.args = {
-  children: [...Array(6 + 1).keys()].slice(1),
-  as: "div",
-  gap: "10px",
-  wrap: "wrap",
-  direction: "row",
-  justifyContent: "unset",
-  alignItems: "initial",
-  alignContent: "unset",
-  height: "100%",
-  width: "100%",
-  margin: "0px",
-  padding: "0px 20px",
-};
-
-export default story;
-
+export default meta;
 export { Default };
--- src/components/StackItem/Stack.stories.tsx
+++ src/components/StackItem/Stack.stories.tsx
@@ -1,39 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Squares } from "../Stack/stories/Squares";
-import { IStackItem, StackItem } from "./index";
+import { StackItem } from "./index";
 import { Stack } from "../Stack";
 import { props } from "./props";
-import { JSX } from "react/jsx-runtime";
 
-const story = {
+const meta = {
   title: "layout/StackItem",
   component: StackItem,
   argTypes: props,
+} satisfies Meta<typeof StackItem>;
+
+type Story = StoryObj<typeof StackItem>;
+
+const Default: Story = {
+  args: {
+    order: 0,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: "auto",
+    alignSelf: "auto",
+  },
+  render: (args) => (
+    <Stack direction="row" gap="20px" wrap="wrap">
+      <StackItem flexGrow={1} flexBasis="300px" {...args}>
+        <Squares item="Item 1" as="div" />
+      </StackItem>
+      <StackItem flexGrow={2} flexBasis="400px" {...args}>
+        <Squares item="Item 2" as="div" />
+      </StackItem>
+      <StackItem flexGrow={1} flexBasis="200px" {...args}>
+        <Squares item="Item 3" as="div" />
+      </StackItem>
+      <StackItem flexGrow={1} flexBasis="250px" {...args}>
+        <Squares item="Item 4" as="div" />
+      </StackItem>
+    </Stack>
+  ),
 };
 
-const Default = (args: JSX.IntrinsicAttributes & IStackItem) => (
-  <Stack direction="row" gap="20px" wrap="wrap">
-    <StackItem flexGrow={1} flexBasis="300px" {...args}>
-      <Squares item="Item 1" as="div" />
-    </StackItem>
-    <StackItem flexGrow={2} flexBasis="400px" {...args}>
-      <Squares item="Item 2" as="div" />
-    </StackItem>
-    <StackItem flexGrow={1} flexBasis="200px" {...args}>
-      <Squares item="Item 3" as="div" />
-    </StackItem>
-    <StackItem flexGrow={1} flexBasis="250px" {...args}>
-      <Squares item="Item 4" as="div" />
-    </StackItem>
-  </Stack>
-);
-
-Default.args = {
-  order: 0,
-  flexGrow: 1,
-  flexShrink: 1,
-  flexBasis: "auto",
-  alignSelf: "auto",
-};
-
-export default story;
+export default meta;
 export { Default };
