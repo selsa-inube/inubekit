4a47bdff0c96e3220c51b18c25f488fb8dfd77ed
Yamil Escorcia Infante <71434195+Yamil2717@users.noreply.github.com>
2025-10-26 14:39:58 -0500

    docs(Colgrup, Pagination, Table, Tbody): stories to new standard

--- src/components/Table/Pagination/Pagination.stories.tsx
+++ src/components/Table/Pagination/Pagination.stories.tsx
@@ -1,29 +1,33 @@
-import { IPagination, Pagination } from ".";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Pagination } from ".";
 
-const story = {
+const meta = {
   title: "data/Pagination",
   component: Pagination,
   argTypes: {
-    firstEntryInPage: { control: "number" },
-    lastEntryInPage: { control: "number" },
-    totalRecords: { control: "number" },
+    firstEntryInPage: { control: { type: "number" as const } },
+    lastEntryInPage: { control: { type: "number" as const } },
+    totalRecords: { control: { type: "number" as const } },
     handleStartPage: { action: "startPage" },
     handlePrevPage: { action: "prevPage" },
     handleNextPage: { action: "nextPage" },
     handleEndPage: { action: "endPage" },
   },
+} satisfies Meta<typeof Pagination>;
+
+type Story = StoryObj<typeof Pagination>;
+
+const Default: Story = {
+  args: {
+    firstEntryInPage: 0,
+    lastEntryInPage: 10,
+    totalRecords: 100,
+    handleStartPage: () => console.log("Start Page clicked"),
+    handlePrevPage: () => console.log("Prev Page clicked"),
+    handleNextPage: () => console.log("Next Page clicked"),
+    handleEndPage: () => console.log("End Page clicked"),
+  },
 };
 
-const Default = (args: IPagination) => <Pagination {...args} />;
-Default.args = {
-  firstEntryInPage: 0,
-  lastEntryInPage: 10,
-  totalRecords: 100,
-  handleStartPage: () => console.log("Start Page clicked"),
-  handlePrevPage: () => console.log("Prev Page clicked"),
-  handleNextPage: () => console.log("Next Page clicked"),
-  handleEndPage: () => console.log("End Page clicked"),
-};
-
+export default meta;
 export { Default };
-export default story;
--- src/components/Table/Tbody/Tbody.stories.tsx
+++ src/components/Table/Tbody/Tbody.stories.tsx
@@ -1,58 +1,63 @@
-import { ITbody, Tbody } from ".";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Tbody } from ".";
 import { Td } from "../Td";
 import { Tr } from "../Tr";
 
-const story = {
+const meta = {
   title: "data/Table/Tbody",
   component: Tbody,
-};
+} satisfies Meta<typeof Tbody>;
 
-const Default = (args: ITbody) => (
-  <table style={{ borderCollapse: "collapse" }}>
-    <Tbody {...args} />
-  </table>
-);
-Default.args = {
-  children: (
-    <>
-      <Tr border="bottom">
-        <Td>Data 1</Td>
-        <Td>Data 2</Td>
-        <Td>Data 3</Td>
-        <Td>Data 4</Td>
-        <Td>Data 5</Td>
-      </Tr>
-      <Tr border="bottom">
-        <Td>Data 1.1</Td>
-        <Td>Data 2.1</Td>
-        <Td>Data 3.1</Td>
-        <Td>Data 4.1</Td>
-        <Td>Data 5.1</Td>
-      </Tr>
-      <Tr border="bottom">
-        <Td>Data 1.2</Td>
-        <Td>Data 2.2</Td>
-        <Td>Data 3.2</Td>
-        <Td>Data 4.2</Td>
-        <Td>Data 5.2</Td>
-      </Tr>
-      <Tr border="bottom">
-        <Td>Data 1.3</Td>
-        <Td>Data 2.3</Td>
-        <Td>Data 3.3</Td>
-        <Td>Data 4.3</Td>
-        <Td>Data 5.3</Td>
-      </Tr>
-      <Tr border="bottom">
-        <Td>Data 1.4</Td>
-        <Td>Data 2.4</Td>
-        <Td>Data 3.4</Td>
-        <Td>Data 4.4</Td>
-        <Td>Data 5.4</Td>
-      </Tr>
-    </>
+type Story = StoryObj<typeof Tbody>;
+
+const Default: Story = {
+  args: {
+    children: (
+      <>
+        <Tr border="bottom">
+          <Td>Data 1</Td>
+          <Td>Data 2</Td>
+          <Td>Data 3</Td>
+          <Td>Data 4</Td>
+          <Td>Data 5</Td>
+        </Tr>
+        <Tr border="bottom">
+          <Td>Data 1.1</Td>
+          <Td>Data 2.1</Td>
+          <Td>Data 3.1</Td>
+          <Td>Data 4.1</Td>
+          <Td>Data 5.1</Td>
+        </Tr>
+        <Tr border="bottom">
+          <Td>Data 1.2</Td>
+          <Td>Data 2.2</Td>
+          <Td>Data 3.2</Td>
+          <Td>Data 4.2</Td>
+          <Td>Data 5.2</Td>
+        </Tr>
+        <Tr border="bottom">
+          <Td>Data 1.3</Td>
+          <Td>Data 2.3</Td>
+          <Td>Data 3.3</Td>
+          <Td>Data 4.3</Td>
+          <Td>Data 5.3</Td>
+        </Tr>
+        <Tr border="bottom">
+          <Td>Data 1.4</Td>
+          <Td>Data 2.4</Td>
+          <Td>Data 3.4</Td>
+          <Td>Data 4.4</Td>
+          <Td>Data 5.4</Td>
+        </Tr>
+      </>
+    ),
+  },
+  render: (args) => (
+    <table style={{ borderCollapse: "collapse" }}>
+      <Tbody {...args} />
+    </table>
   ),
 };
 
+export default meta;
 export { Default };
-export default story;
